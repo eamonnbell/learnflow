df90cd0b039f673284ac38d7146f946562c3a6c5
Eamonn Bell <pseudoboarder@gmail.com>
2017-07-13 09:42:14 +0100

    Fix whitespace.

--- app/js/router.js
+++ app/js/router.js
@@ -5,49 +5,49 @@ Backbone.$ = $;
 const views = require('./views/views');
 
 module.exports = Backbone.Router.extend({
-    routes: {
-        'home': 'home',
-        'signup': 'signup',
-        'login': 'login',
-        'trees/all': 'tree',
-        'nodes/all': 'nodelist',
-        'votes/all': 'votelist',
-        'voting': 'voting',
-        '*path': 'home'
-    },
+  routes: {
+    'home': 'home',
+    'signup': 'signup',
+    'login': 'login',
+    'trees/all': 'tree',
+    'nodes/all': 'nodelist',
+    'votes/all': 'votelist',
+    'voting': 'voting',
+    '*path': 'home'
+  },
 
-    home: function(){
-        var view = new views.Home();
-        $('#main').html(view.render().el);
-    },
+  home: function(){
+    var view = new views.Home();
+    $('#main').html(view.render().el);
+  },
 
-    signup: function(){
-        var view = new views.Signup();
-        $('#main').html(view.render().el);
-    },
+  signup: function(){
+    var view = new views.Signup();
+    $('#main').html(view.render().el);
+  },
 
-    login: function(){
-        var view = new views.Login();
-        $('#main').html(view.render().el);
-    },
+  login: function(){
+    var view = new views.Login();
+    $('#main').html(view.render().el);
+  },
 
-    nodelist: function(){
-        var view = new views.NodeList();
-        $('#main').html(view.render().el);
-    },
+  nodelist: function(){
+    var view = new views.NodeList();
+    $('#main').html(view.render().el);
+  },
 
-    votelist: function(){
-        var view = new views.VoteList();
-        $('#main').html(view.render().el);
-    },
+  votelist: function(){
+    var view = new views.VoteList();
+    $('#main').html(view.render().el);
+  },
 
-    voting: function(){
-        var view = new views.Voting();
-        $('#main').html(view.render().el);
-    },
+  voting: function(){
+    var view = new views.Voting();
+    $('#main').html(view.render().el);
+  },
 
-    tree: function(){
-        var view = new views.TreeView();
-        $('#main').html(view.render().el);
-    }
+  tree: function(){
+    var view = new views.TreeView();
+    $('#main').html(view.render().el);
+  }
 });
